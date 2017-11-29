ca79b449464a0bca896865c6cc2e34d34e0aee4d
Ericka Oviedo <erickaov@gmail.com>
2017-11-28 23:48:40 -0600

    Add "Shape" component.

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,35 @@
-$(document).ready(function() {
+/* global $ */
+
+$(document).ready(function () {
+    /*
+     * Tab animations
+     */
     $(".menu .item").tab();
+    
+    /*
+     * Shape animations
+     */
+    $(".flip-up").on("click", function () {
+        $(".shape").shape("flip up");
+    });
+    
+    $(".flip-down").on("click", function () {
+        $(".shape").shape("flip down");
+    });
+    
+    $(".flip-left").on("click", function () {
+        $(".shape").shape("flip left");
+    });
+    
+    $(".flip-right").on("click", function () {
+        $(".shape").shape("flip right");
+    });
+    
+    $(".flip-over").on("click", function () {
+        $(".shape").shape("flip over");
+    });
+    
+    $(".flip-back").on("click", function () {
+        $(".shape").shape("flip back");
+    });
 });
